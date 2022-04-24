6e1a166385acccd32470a25ef692e2e8de6e3e47
liliyagerenko <liliyaogerenko@gmail.com>
2022-04-24 13:47:28 -0700

    imagename

--- src/layout/gameImage/index.jsx
+++ src/layout/gameImage/index.jsx
@@ -5,128 +5,128 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 const dataArray = [
   {
-    gamelogo: "game_imgs+logos/Games_imgs/8ball.png",
-    gameimage: "game_imgs+logos/Games_imgs/8ball.png",
+    gamelogo: "gameimagelogo/Games_imgs/8ball.png",
+    gameimage: "gameimagelogo/Games_imgs/8ball.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/apex.png",
-    gameimage: "game_imgs+logos/Games_imgs/apex.png",
+    gamelogo: "gameimagelogo/Games_imgs/apex.png",
+    gameimage: "gameimagelogo/Games_imgs/apex.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/asphalt.png",
-    gameimage: "game_imgs+logos/Games_imgs/asphalt.png",
+    gamelogo: "gameimagelogo/Games_imgs/asphalt.png",
+    gameimage: "gameimagelogo/Games_imgs/asphalt.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/BGMI.png",
-    gameimage: "game_imgs+logos/Games_imgs/BGMI.png",
+    gamelogo: "gameimagelogo/Games_imgs/BGMI.png",
+    gameimage: "gameimagelogo/Games_imgs/BGMI.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/brawl_stars.png",
-    gameimage: "game_imgs+logos/Games_imgs/brawl_stars.png",
+    gamelogo: "gameimagelogo/Games_imgs/brawl_stars.png",
+    gameimage: "gameimagelogo/Games_imgs/brawl_stars.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/BRAWLHALLA.png",
-    gameimage: "game_imgs+logos/Games_imgs/BRAWLHALLA.png",
+    gamelogo: "gameimagelogo/Games_imgs/BRAWLHALLA.png",
+    gameimage: "gameimagelogo/Games_imgs/BRAWLHALLA.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/CLASH_mini.png",
-    gameimage: "game_imgs+logos/Games_imgs/CLASH_mini.png",
+    gamelogo: "gameimagelogo/Games_imgs/CLASH_mini.png",
+    gameimage: "gameimagelogo/Games_imgs/CLASH_mini.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/clash_of_clans.png",
-    gameimage: "game_imgs+logos/Games_imgs/clash_of_clans.png",
+    gamelogo: "gameimagelogo/Games_imgs/clash_of_clans.png",
+    gameimage: "gameimagelogo/Games_imgs/clash_of_clans.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/clash-royale 2.png",
-    gameimage: "game_imgs+logos/Games_imgs/clash-royale 2.png",
+    gamelogo: "gameimagelogo/Games_imgs/clash-royale 2.png",
+    gameimage: "gameimagelogo/Games_imgs/clash-royale 2.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/CODGeneric.png",
-    gameimage: "game_imgs+logos/Games_imgs/CODGeneric.png",
+    gamelogo: "gameimagelogo/Games_imgs/CODGeneric.png",
+    gameimage: "gameimagelogo/Games_imgs/CODGeneric.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/CSGO.png",
-    gameimage: "game_imgs+logos/Games_imgs/CSGO.png",
+    gamelogo: "gameimagelogo/Games_imgs/CSGO.png",
+    gameimage: "gameimagelogo/Games_imgs/CSGO.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/fifa21.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/fifa21.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/fifa21.jpg",
+    gameimage: "gameimagelogo/Games_imgs/fifa21.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/FIFA22.png",
-    gameimage: "game_imgs+logos/Games_imgs/FIFA22.png",
+    gamelogo: "gameimagelogo/Games_imgs/FIFA22.png",
+    gameimage: "gameimagelogo/Games_imgs/FIFA22.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/fortnite.png",
-    gameimage: "game_imgs+logos/Games_imgs/fortnite.png",
+    gamelogo: "gameimagelogo/Games_imgs/fortnite.png",
+    gameimage: "gameimagelogo/Games_imgs/fortnite.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/freefire.png",
-    gameimage: "game_imgs+logos/Games_imgs/freefire.png",
+    gamelogo: "gameimagelogo/Games_imgs/freefire.png",
+    gameimage: "gameimagelogo/Games_imgs/freefire.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/guilty.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/guilty.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/guilty.jpg",
+    gameimage: "gameimagelogo/Games_imgs/guilty.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/HALO infinite.png",
-    gameimage: "game_imgs+logos/Games_imgs/HALO infinite.png",
+    gamelogo: "gameimagelogo/Games_imgs/HALO infinite.png",
+    gameimage: "gameimagelogo/Games_imgs/HALO infinite.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/karunker.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/karunker.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/karunker.jpg",
+    gameimage: "gameimagelogo/Games_imgs/karunker.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/leagueoflegends.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/leagueoflegends.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/leagueoflegends.jpg",
+    gameimage: "gameimagelogo/Games_imgs/leagueoflegends.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/ludo.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/ludo.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/ludo.jpg",
+    gameimage: "gameimagelogo/Games_imgs/ludo.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/madden.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/madden.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/madden.jpg",
+    gameimage: "gameimagelogo/Games_imgs/madden.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/mobile_legends.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/mobile_legends.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/mobile_legends.jpg",
+    gameimage: "gameimagelogo/Games_imgs/mobile_legends.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/nba22.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/nba22.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/nba22.jpg",
+    gameimage: "gameimagelogo/Games_imgs/nba22.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/overwatch.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/overwatch.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/overwatch.jpg",
+    gameimage: "gameimagelogo/Games_imgs/overwatch.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/pokemon.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/pokemon.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/pokemon.jpg",
+    gameimage: "gameimagelogo/Games_imgs/pokemon.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/Rainbow Six.png",
-    gameimage: "game_imgs+logos/Games_imgs/Rainbow Six.png",
+    gamelogo: "gameimagelogo/Games_imgs/Rainbow Six.png",
+    gameimage: "gameimagelogo/Games_imgs/Rainbow Six.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/Rocket League.png",
-    gameimage: "game_imgs+logos/Games_imgs/Rocket League.png",
+    gamelogo: "gameimagelogo/Games_imgs/Rocket League.png",
+    gameimage: "gameimagelogo/Games_imgs/Rocket League.png",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/supersmash_bro.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/supersmash_bro.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/supersmash_bro.jpg",
+    gameimage: "gameimagelogo/Games_imgs/supersmash_bro.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/tekken7.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/tekken7.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/tekken7.jpg",
+    gameimage: "gameimagelogo/Games_imgs/tekken7.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/valorant.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/valorant.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/valorant.jpg",
+    gameimage: "gameimagelogo/Games_imgs/valorant.jpg",
   },
   {
-    gamelogo: "game_imgs+logos/Games_imgs/wildrift.jpg",
-    gameimage: "game_imgs+logos/Games_imgs/wildrift.jpg",
+    gamelogo: "gameimagelogo/Games_imgs/wildrift.jpg",
+    gameimage: "gameimagelogo/Games_imgs/wildrift.jpg",
   },
 ];
 
@@ -162,16 +162,6 @@ const GameImage = () => {
             />
           </Stack>
         </div>
-
-        {/* <div className="game-image">
-            <img src="./game_imgs+logos/Games_imgs/HALO infinite.png" alt="" id="imgSize"/>
-        </div>
-        <div className="game-image">
-            <img src="./game_imgs+logos/Games_imgs/HALO infinite.png" alt="" id="imgSize"/>
-        </div>
-        <div className="game-image">
-            <img src="./game_imgs+logos/Games_imgs/HALO infinite.png" alt="" id="imgSize"/>
-        </div> */}
       </div>
     </>
   );
